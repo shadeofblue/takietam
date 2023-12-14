6518e01eb78bcd77000c7fa1c0c8545093b91517
shadeofblue <blue@golem.network>
2023-12-14 11:35:58 +0100

    async condtion

--- async_condition.py
+++ async_condition.py
@@ -1,5 +1,6 @@
 import asyncio
 
+some_counter = 0
 
 async def cwait(c: asyncio.Condition, m: str):
     async with c:
@@ -12,8 +13,24 @@ async def counter(n: int):
         await asyncio.sleep(1)
         print("counter: ", nstart - i)
 
+async def inc_some_counter():
+    global some_counter
+    while True:
+        some_counter += 1
+        await asyncio.sleep(0.001)
+
+async def wait_come_counter(c: asyncio.Condition, value):
+    async with c:
+        await c.wait_for(lambda: some_counter > value)
+        print("finished waiting for:", value, " actual: ", some_counter)
+
 async def main():
     c = asyncio.Condition()
+
+    asyncio.create_task(inc_some_counter())
+
+    asyncio.create_task(wait_come_counter(c, 100))
+    asyncio.create_task(wait_come_counter(c, 1000))
 
     for i in range(10):
         asyncio.create_task(cwait(c, str(i+1)))
